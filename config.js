e4689cd33d623dddc8303a216768f4c4b7bea2f6
Lauriel Mesquita <laurielmesquita@me.com>
2020-09-23 19:23:55 -0300

    :building_construction: Mail information changed

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ module.exports = {
     {
       icon: 'fa-envelope-o',
       name: 'Email',
-      url: 'mailto:diretoria@jupialimentos.com'
+      url: 'mailto:jupi@jupialimentos.com.br'
     }
   ]
 }
